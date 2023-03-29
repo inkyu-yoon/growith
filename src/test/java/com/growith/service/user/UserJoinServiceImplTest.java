9c2ae9654d50ae50310615b7801fa3e27a3dcf6b
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-03-29 14:25:26 +0900

    [test] : User Join Service 테스트 코드 작성

---
+++ src/test/java/com/growith/service/user/UserJoinServiceImplTest.java
@@ -0,0 +1,94 @@
+package com.growith.service.user;
+
+import com.growith.domain.user.User;
+import com.growith.domain.user.UserRepository;
+import com.growith.domain.user.UserRole;
+import com.growith.domain.user.oauth.UserProfile;
+import com.growith.global.util.JwtUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.internal.MockedStaticImpl;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserJoinServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private User mockUser;
+
+    @InjectMocks
+    private UserJoinServiceImpl userJoinService;
+
+    @Nested
+    @DisplayName("회원 회원가입 · 로그인 테스트")
+    class userJoinLoginTest{
+
+        @Mock
+        UserProfile userProfile;
+
+        @Test
+        @DisplayName("로그인 성공 (회원가입 안되어있는 경우 회원 가입 후 jwt토큰 발급)")
+        public void userLoginSuccess(){
+
+            MockedStatic<JwtUtil> jwtUtilMockedStatic = mockStatic(JwtUtil.class);
+            String email = "email";
+
+            given(userProfile.getEmail())
+                    .willReturn(email);
+            given(userRepository.findByEmail(email))
+                    .willReturn(Optional.empty());
+            given(userRepository.saveAndFlush(userProfile.toEntity()))
+                    .willReturn(mockUser);
+            given(mockUser.getUserRole())
+                    .willReturn(UserRole.ROLE_USER);
+            given(JwtUtil.createToken(anyString(), anyString(), anyString(), anyLong())).
+                    willReturn("jwt");
+
+
+            assertDoesNotThrow(() -> userJoinService.login(userProfile));
+
+            verify(userRepository, atLeastOnce()).findByEmail(email);
+            verify(userRepository, atLeastOnce()).saveAndFlush(userProfile.toEntity());
+
+            jwtUtilMockedStatic.close();
+        }
+        @Test
+        @DisplayName("로그인 성공 (회원가입 되어있는 경우 jwt토큰 발급)")
+        public void userLoginSuccess1(){
+
+            MockedStatic<JwtUtil> jwtUtilMockedStatic = mockStatic(JwtUtil.class);
+            String email = "email";
+
+            given(userProfile.getEmail())
+                    .willReturn(email);
+            given(userRepository.findByEmail(email))
+                    .willReturn(Optional.of(mockUser));
+
+            given(mockUser.getUserRole())
+                    .willReturn(UserRole.ROLE_USER);
+            given(JwtUtil.createToken(anyString(), anyString(), anyString(), anyLong())).
+                    willReturn("jwt");
+
+
+            assertDoesNotThrow(() -> userJoinService.login(userProfile));
+
+            verify(userRepository, atLeastOnce()).findByEmail(email);
+
+            jwtUtilMockedStatic.close();
+        }
+    }
+
+}
